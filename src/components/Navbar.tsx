800b1ad53cf00adc9e2e8b59b06ffda6aa990ddf
Abhishek-90 <holaniabhishek90@gmail.com>
2023-02-12 18:52:43 +0530

    navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,44 @@
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+// import Logo from "";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        gap: { sm: "122px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        px: "20px",
+      }}
+    >
+      <Link to="/">
+        <img
+          src="/assets/images/Logo.png"
+          alt=""
+          style={{ width: "48px", height: "48px", margin: "0 20px" }}
+        />
+      </Link>
+      <Stack direction="row" gap={"40px"} fontSize="24px" alignItems="flex-end">
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#3a1212",
+            borderBottom: "3px solid #ff2625",
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: "none", color: "#3a1212" }}
+        >
+          Exercises
+        </a>
+      </Stack>
+    </Stack>
+  );
 }
 
 export default Navbar;
