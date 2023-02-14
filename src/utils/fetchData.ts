99194338326ca37d15fb32ecd1488a3e37653763
Abhishek-90 <holaniabhishek90@gmail.com>
2023-02-15 00:07:12 +0530

    new utility function

---
+++ src/utils/fetchData.ts
@@ -0,0 +1,14 @@
+export const exerciseOption = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+async function fetchData(url: any, options: any) {
+  const response = await fetch(url, options);
+  const json = await response.json();
+  return json;
+}
+
+export default fetchData;
