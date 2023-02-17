1dbbfc1ee50d930b2886d58c04b763c090cd1ea1
Abhishek-90 <holaniabhishek90@gmail.com>
2023-02-18 01:03:21 +0530

    youtube options added

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,19 @@
 export const exerciseOption = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_EXERCISE_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY,
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
 async function fetchData(url, options) {
   const response = await fetch(url, options);
   const json = await response.json();
