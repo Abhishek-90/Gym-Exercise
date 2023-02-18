3178bc3a79e34be69162f58c761a68d3d8d5af97
Abhishek-90 <holaniabhishek90@gmail.com>
2023-02-18 18:45:50 +0530

    footer created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,16 @@
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/Logo-1.png";
 function Footer() {
-  return <></>;
+  return (
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="30px" px="40px" pt="20px" sx={{ alignItems: "center" }}>
+        <img src={Logo} alt="logo" width="200px" height="40px" loading="lazy" />
+        <Typography variant="h5" mb="30px">
+          Made with ❤️ By Abhishek Holani.
+        </Typography>
+      </Stack>
+    </Box>
+  );
 }
 
 export default Footer;
