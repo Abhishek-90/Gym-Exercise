b22f5a2d19e2553c7d781096e6c8647f6d152ea6
Abhishek-90 <holaniabhishek90@gmail.com>
2023-02-18 14:26:22 +0530

    exercise video done

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const ExerciseVideos = () => {
-  return <div>ExerciseVideos</div>;
+const ExerciseVideos = ({ exerciseVideos, name }) => {
+  if (!exerciseVideos.length) return "Loading...";
+  return (
+    <Box sx={{ marginTop: { lg: "200px", xs: "20px" }, p: "20px" }}>
+      <Typography variant="h4" mb="33px">
+        Watch{" "}
+        <span style={{ color: "#ff2625", textTransform: "capitalize" }}>
+          {name}
+        </span>{" "}
+        exercise videos
+      </Typography>
+      <Stack
+        alignItems="center"
+        flexWrap="wrap"
+        justifyContent="flex-start"
+        sx={{
+          flexDirection: { lg: "row" },
+          gap: { lg: "110px", xs: 0 },
+        }}
+      >
+        {exerciseVideos?.slice(0, 3).map((video, index) => (
+          <a
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${video.video.videoId}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={video.video.thumbnails[0].url} alt={video.video.title} />
+          </a>
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default ExerciseVideos;
