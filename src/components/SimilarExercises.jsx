24f2a35dbbd8acc705ad1dd55ce455e097d494fb
Abhishek-90 <holaniabhishek90@gmail.com>
2023-02-18 18:30:28 +0530

    exercise Details page finished

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import HorizontalScrollBar from "./HorizontalScrollBar";
+import Loader from "./Loader";
 
-const SimilarExercises = () => {
-  return <div>SimilarExercises</div>;
+const SimilarExercises = ({
+  similarTargetExercise,
+  similarEquipmentExercise,
+}) => {
+  return (
+    <Box sx={{ mt: { lg: "100px", xs: "0" }, p: "20px" }}>
+      <Typography variant="h4">
+        Exercises that target same muscle group
+      </Typography>
+      <Stack
+        gap="70px"
+        flexWrap="wrap"
+        justifyContent="center"
+        sx={{
+          position: "relative",
+          p: "20px",
+          mt: { lg: "50px", xs: "20px" },
+          mb: { lg: "50px", xs: "20px" },
+        }}
+        direction="row"
+      >
+        {similarTargetExercise.length > 0 ? (
+          <HorizontalScrollBar
+            isBodyPart={false}
+            data={similarTargetExercise}
+          />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+      <Typography variant="h4">Exercises that use same Equipments</Typography>
+      <Stack
+        gap="70px"
+        flexWrap="wrap"
+        justifyContent="center"
+        sx={{
+          position: "relative",
+          p: "20px",
+          mt: { lg: "50px", xs: "20px" },
+          mb: { lg: "50px", xs: "20px" },
+        }}
+        direction="row"
+      >
+        {similarEquipmentExercise.length > 0 ? (
+          <HorizontalScrollBar
+            isBodyPart={false}
+            data={similarEquipmentExercise}
+          />
+        ) : (
+          <Loader></Loader>
+        )}
+      </Stack>
+    </Box>
+  );
 };
 
 export default SimilarExercises;
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,5 +1,64 @@
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Details from "../components/Details";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+import fetchData, { exerciseOption, youtubeOptions } from "../utils/fetchData";
+
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideoData, setExerciseVideoData] = useState({});
+  const [similarTargetExercise, setSimilarTargetExercise] = useState({});
+  const [similarEquipmentExercise, setSimilarEquipmentExercise] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExerciseDetails = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseData = await fetchData(
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
+        exerciseOption
+      );
+      setExerciseDetail(exerciseData);
+
+      const exerciseVideoData = await fetchData(
+        `${youtubeSearchUrl}/search?query=${exerciseData?.name}`,
+        youtubeOptions
+      );
+      setExerciseVideoData(exerciseVideoData.contents);
+
+      const similarTargetExerciseData = await fetchData(
+        `${exerciseDbUrl}/exercises/target/${exerciseData?.target}`,
+        exerciseOption
+      );
+      setSimilarTargetExercise(similarTargetExerciseData);
+
+      const similarEquipmentExerciseData = await fetchData(
+        `${exerciseDbUrl}/exercises/equipment/${exerciseData?.equipment}`,
+        exerciseOption
+      );
+      setSimilarEquipmentExercise(similarEquipmentExerciseData);
+    };
+    fetchExerciseDetails();
+  }, [id]);
+
+  return (
+    <Box>
+      <Details exerciseDetail={exerciseDetail} />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideoData}
+        name={exerciseDetail.name}
+      />
+      <SimilarExercises
+        similarTargetExercise={similarTargetExercise}
+        similarEquipmentExercise={similarEquipmentExercise}
+      />
+    </Box>
+  );
 };
 
 export default ExerciseDetail;
